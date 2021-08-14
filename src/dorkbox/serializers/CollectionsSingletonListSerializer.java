91ea197a0c85fa936545c965ce67567f669f57ce
Robinson <robinson@dorkbox.com>
2021-08-14 14:51:35 -0600

    To LF endings

--- src/dorkbox/serializers/CollectionsSingletonListSerializer.java
+++ src/dorkbox/serializers/CollectionsSingletonListSerializer.java
@@ -16,13 +16,13 @@
  */
 package dorkbox.serializers;
 
+import java.util.Collections;
+import java.util.List;
+
 import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryo.Serializer;
 import com.esotericsoftware.kryo.io.Input;
 import com.esotericsoftware.kryo.io.Output;
-
-import java.util.Collections;
-import java.util.List;
 
 /**
  * A kryo {@link Serializer} for {@link List}s created via {@link Collections#singletonList(Object)}.
